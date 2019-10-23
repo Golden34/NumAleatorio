d2f83b87796435295c35a28b6566567e57e5ef70
Golden34 <golden34@gmail.com>
2019-10-24 00:11:17 +0200

    Primera subida

---
+++ app/src/main/java/com/example/numaleatorio/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.numaleatorio;
+
+import androidx.annotation.ColorRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+        TextView titleView = new TextView(this);
+        titleView.setWidth(LinearLayout.LayoutParams.WRAP_CONTENT);
+        titleView.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+        titleView.setText("Hallo Welt!");
+        titleView.setVisibility(View.VISIBLE);
+
+        FrameLayout myRoot = findViewById(R.id.cajroot);
+        LinearLayout a = new LinearLayout(this);
+        a.setOrientation(LinearLayout.VERTICAL);
+        a.addView(titleView);
+        //a.addView(titleView);
+
+        myRoot.addView(a); */
+
+    }
+
+    public void bPulsado(View v) {
+        //entrada de inicio y fin
+        int ini = 1;
+        int fin = 72;
+
+        //deducir la cantida de numero entre medias con ellos incluidos
+        int dif = fin - ini;  // 72 - 34 = 38
+        int r = dif%7;
+        int z = ini;
+
+//        LinearLayout layout_y = findViewById(R.id.dyn_layoutY);
+//        LinearLayout layout_x = findViewById(R.id.dyn_layoutX);
+
+        LinearLayout Layout_XML = findViewById(R.id.xml_layout);
+        for (int y = 0; y < 10; y++) {
+            LinearLayout layout_y = GeneraLinearLayout(LinearLayout.HORIZONTAL);
+            for (int x = 0; x < 10; x++) {
+                LinearLayout layout_x = GeneraLinearLayout(LinearLayout.VERTICAL);
+                layout_x.addView(GeneraCaja(z));
+                layout_y.addView(layout_x);
+                z++;
+            }
+            Layout_XML.addView(layout_y);
+        }
+        System.out.println("**** Adding text view " + dif);
+    }
+
+    public LinearLayout GeneraLinearLayout(int Orientacion){
+        LinearLayout LL = new LinearLayout(this);
+        LL.setOrientation(Orientacion);
+        if (Orientacion == LinearLayout.HORIZONTAL ){
+            LL.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.FILL_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+            LL.setPadding(10, 10, 10, 10);
+        }
+
+        return LL;
+    }
+    public TextView GeneraCaja(int i){
+        TextView text = new TextView(this);
+        text.setText("" + i);
+        text.setTextSize(12);
+        text.setGravity(Gravity.LEFT);
+        text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        text.setBackgroundColor(R.color.colorPrimary);
+        return text;
+    }
+}
