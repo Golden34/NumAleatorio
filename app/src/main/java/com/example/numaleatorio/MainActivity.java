45aa4d7ecce8cd95d1f135ee37678125ba35600d
Golden34 <golden34@gmail.com>
2019-10-25 18:11:03 +0200

    con error subsanable

--- app/src/main/java/com/example/numaleatorio/MainActivity.java
+++ app/src/main/java/com/example/numaleatorio/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.numaleatorio.R.color.colorAccent;
+import static com.example.numaleatorio.R.color.colorGreen;
+import static com.example.numaleatorio.R.color.colorYellow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,49 +26,36 @@ public class MainActivity extends AppCompatActivity {
     public int adi;
     public TextView tv[];
     public int total;
+    int colorRed;
+    int colorYel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        total = (topey*topex+1);
+        colorRed = getResources().getColor(colorAccent);
+        colorYel = getResources().getColor(colorYellow);
+
+        total = (topey*topex);
         tv = new TextView[total];
-
-        /*
-        TextView titleView = new TextView(this);
-        titleView.setWidth(LinearLayout.LayoutParams.WRAP_CONTENT);
-        titleView.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
-        titleView.setText("Hallo Welt!");
-        titleView.setVisibility(View.VISIBLE);
-
-        FrameLayout myRoot = findViewById(R.id.cajroot);
-        LinearLayout a = new LinearLayout(this);
-        a.setOrientation(LinearLayout.VERTICAL);
-        a.addView(titleView);
-        //a.addView(titleView);
-
-        myRoot.addView(a); */
 
     }
 
     public void bPulsado(View v) {
         //entrada de inicio y fin
 
-
         //deducir la cantida de numero entre medias con ellos incluidos
         int dif = fin - ini;  // 72 - 34 = 38
         int r = dif%7;
         int z = ini;
 
-//        LinearLayout layout_y = findViewById(R.id.dyn_layoutY);
-//        LinearLayout layout_x = findViewById(R.id.dyn_layoutX);
-
         LinearLayout Layout_XML = findViewById(R.id.xml_layout);
+        Layout_XML.setPadding(10, 10, 10, 10);
         for (int y = 0; y < topey; y++) {
             LinearLayout layout_y = GeneraLinearLayout(LinearLayout.HORIZONTAL);
             for (int x = 0; x < topex; x++) {
-                LinearLayout layout_x = GeneraLinearLayout(LinearLayout.VERTICAL);
+                LinearLayout layout_x = GeneraLinearLayout(LinearLayout.HORIZONTAL);
                 layout_x.addView(GeneraCaja(y, x));
                 layout_y.addView(layout_x);
                 z++;
@@ -77,22 +70,46 @@ public class MainActivity extends AppCompatActivity {
         LL.setOrientation(Orientacion);
         if (Orientacion == LinearLayout.HORIZONTAL ){
             LL.setLayoutParams(new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.FILL_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT));
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT, 3F));
+        }
+        else{
+            LL.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT, 2F));
+            LL.setBackgroundColor(colorAccent);
             LL.setPadding(10, 10, 10, 10);
         }
 
+
         return LL;
     }
+    @SuppressLint("ResourceAsColor")
     public TextView GeneraCaja(int ty, int tx){
-        int posi = (ty*10 + tx + 1);
+        int posi = (ty*10 + tx);
+        String cero;
         TextView text = new TextView(this);
-        tv[posi] = text;
-        text.setText(" " + posi + " ");
-        text.setTextSize(12);
-        text.setGravity(Gravity.LEFT);
+        if (posi<10) cero = "  0"; else cero = "  ";
+        String sNum = cero + posi;
+        text.setText(sNum);
+        text.setTextSize(22);
+        text.setGravity(Gravity.START);
         text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        text.setBackgroundColor(R.color.colorPrimary);
+        text.setBackgroundColor(colorRed);
+        text.setTextColor(colorYel);
+        text.setPadding(2, 2, 2, 2);
+        text.setOnClickListener(
+                View.OnClickListener()
+                onClick(text));
+//                    public void onClick(View v)
+//                    {
+//                        //String sNum = (TextView)v.getTag();
+//                    }
+//                }
+//        );
+
+        tv[posi] = text;
+        onClick(text);
         return text;
     }
 
@@ -100,12 +117,14 @@ public class MainActivity extends AppCompatActivity {
     public void bPulsado2(View view) {
         for (int z = 1; z < total; z++) {
             TextView text = (tv[z]);
-            if (z < ini) text.setVisibility(View.INVISIBLE);
-            if (z > fin) text.setVisibility(View.INVISIBLE);
+            if (z < ini || z > fin) tv[z].setVisibility(View.INVISIBLE);
+
         }
     }
 
-
+    public void onClick(TextView Caja_Tocada){
+        Toast.makeText(this," ha tocado el numero ", Toast.LENGTH_LONG);
+    }
 
     /*
         En el Onclick tiene que ver si ha acertado:
